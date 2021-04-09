669fa1751ee9b590bbbefb2238f156ed95d48e65
Tamilyn <tamilyndee@gmail.com>
2021-04-09 15:30:00 -0600

    Mid-day 4/9 -Tam

--- Mummies/wwwroot/js/site.js
+++ Mummies/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function openMummy(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function closeMummy(id) {
+    document.getElementByName(id).style.display = "none";
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "400px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
